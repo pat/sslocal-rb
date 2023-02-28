d5d964959435bfd238edb3289eba0b1850e76b62
Jared Moody <jared@jaredmoody.com>
2023-02-28 15:56:18 -0800

    Fixes #1 - Skip Webpacker railtie when Webpacker not present

--- lib/sslocal/railtie.rb
+++ lib/sslocal/railtie.rb
@@ -5,6 +5,7 @@ require "sslocal/state"
 module SSLocal
   class Railtie < Rails::Railtie
     initializer "sslocal.set_up_webpacker", :before => "webpacker.proxy" do
+      next unless defined?(Webpacker)
       state = SSLocal::State.new(Rails.env.to_s)
       Webpacker.config.dev_server[:https] = true if state.enabled?
     end
